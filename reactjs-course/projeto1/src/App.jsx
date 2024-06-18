97c62febab6878477d11bf9b9bbddc140030b236
Daniel <dangosil@yahoo.com.br>
2024-06-18 13:13:15 -0300

    update reactjs-course projeto1

--- reactjs-course/projeto1/src/App.jsx
+++ reactjs-course/projeto1/src/App.jsx
@@ -7,6 +7,10 @@ import { Navbar } from "./components/Navbar/Navbar"; // POSSO DAR QUALQUER NOME 
 
 import './styles/App.css'
 
+import article1Img from './assets/images/article1.png'
+import article2Img from './assets/images/article2.png'
+import article3Img from './assets/images/article3.png'
+
 
 // Componente em classe é uma classe que herda a classe componente do REACT, e retorna HTML dentro do método render.
 
@@ -20,7 +24,14 @@ class App extends React.Component {
         <Navbar />
 
         <section id="articles">
-          <Article />
+          <Article title="Designing Dashboards" provider="NASA" description="1 Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet." thumbnail={article1Img} />
+
+          <Article title="Vibrant Portraits of 2020" provider="SpaceNews" description="2 Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet." thumbnail={article2Img} />
+
+          <Article title="36 Days of Malayalam type" provider="Spaceflight Now" description="3 Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet." thumbnail={article3Img} />
+
+          <Article title="Designing Dashboards" provider="NASA" description="4 Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet." thumbnail={article1Img} />
+
         </section>
       </>
     );
--- reactjs-course/projeto1/src/components/Article/Article.jsx
+++ reactjs-course/projeto1/src/components/Article/Article.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 
-import articleImg from '../../assets/images/article1.png'
+import './styles.css';
+
+/*  this.props = {
+        title: "Exemplo"
+        provider: "NASA"
+    }   
+*/ 
 
 export class Article extends React.Component {
     render() {
         return (
-            <article>
-                <img src={articleImg} alt="" />
+            <article id="article">
+                <img src={this.props.thumbnail} alt="{this.props.title}" />
 
                 <div className="article-infos">
-                    <h2>Designing Dashboards</h2>
-                    <h3>NASA</h3>
+                    <h2>{this.props.title}</h2>
+                    <h3>{this.props.provider}</h3>
 
-                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+                    <p>{this.props.description}</p>
                 </div>
             </article>
         )
